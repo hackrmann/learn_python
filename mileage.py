cdfb69ebbc11755c2a8ebbc7516d3d4e1985f94d
Thejus <thejus44@gmail.com>
2018-08-11 01:32:57 +0530

    Finished variables and strings

---
+++ mileage.py
@@ -0,0 +1,4 @@
+print("How many kilometeres did you run today? ")
+x = int(input())
+print(f"You ran {x/1.6} miles today!")
+# count( thing to round off to , number of decimal places to round off to )
---
+++ random_functions.py
@@ -0,0 +1,5 @@
+type(4)                                                                         #Gives you the type of data passed through the parenthesis
+a = 5                                                                           #Assigns value of 5 to a
+b = "Hello"                                                                     #Sets variable b to "Hello"
+c,d,e = 1,"hello",False                                                         #Multiple declaration of variables
+# count( thing to round off to , number of decimal places to round off to )
