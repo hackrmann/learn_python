9c94e4ee858cecc9894ea0167088f4254ab1cc6f
Thejus <thejus44@gmail.com>
2018-08-12 16:45:45 +0530

    Added conditinonal statements to print10

--- print10.py
+++ print10.py
@@ -8,3 +8,10 @@ print('hello!')
 print('hello!')
 print('hello!')
 print('hello!')
+o = 'hello'
+if o == 'hello':
+    print(o)
+elif o =='nested':
+        print("nested")
+else:
+    print('Nothing happens')
