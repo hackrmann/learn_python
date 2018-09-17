8297b3f7a1d0165196a0257991f2a148797d77f5
Thejus <thejus44@gmail.com>
2018-09-18 01:44:42 +0530

    Just doing it to get rid of the green dialogue box

--- lists.py
+++ lists.py
@@ -3,7 +3,7 @@ len(demo_list)                  #Gives the length of the list
 list_of_nos = list(range(1,11)) #Makes list of numbers 1 to 10. Example of using list as a function
 
 print(list_of_nos)
-print(str(list_of_nos))
+print(str(list_of_nos)[0])
 print(demo_list[-1])
 string_list = list(demo_list[3])
 print(string_list)
@@ -30,3 +30,13 @@ demo_list.pop(1)                    # returns element at index 1
 print(demo_list)
 demo_list.remove(3)                 #Does not remove. Different from del
 print(demo_list)
+
+print(list_of_nos.index(4))         #Return the index of 4 in the list "list_of_nos"
+print(list_of_nos.index(4,1))       #Returns the index of 4 after the index of 1 (inclusive of 1)
+demo_list.extend([5,6,9,7,411])
+print(demo_list.index(5,0,3))       #Does not return index 3
+dum = str(list_of_nos)
+print(dum[5])                       #Returns a ,
+print(list_of_nos.reverse())
+print(list_of_nos)
+new_st = '-'.join(list_of_nos)
